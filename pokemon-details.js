f5d21edf19f0ee6f3b4989bd5ae465fb33d983d8
Byakuya91 <nganti91@yahoo.com>
2025-04-13 10:20:13 -0400

    I've fixed a reference error for the pokemon-details page for Axios. Coded the color background and text functions. Moving onto coding functions to captialize the first letter and display the pokemon.

--- pokemon-details.js
+++ pokemon-details.js
@@ -72,3 +72,107 @@ const loadPokemon = async (id) => {
     return false;
   }
 };
+
+// ?Function to navigate pokemon
+const navigatePokemon = async (id) => {
+  // ? get the current pokemon id and change it to the new one
+  currentPokemonID = id;
+  await loadPokemon(id);
+};
+
+// ? object to hold all the BG colors for the pokedex
+const typeColors = {
+  normal: "#A8A77A",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+};
+
+// ? function to set the element styles
+
+const setElementStyles = (elements, cssProperties, value) => {
+  elements.forEach((element) => {
+    //   attach the element styles to all the elements
+    element.style[cssProperties] = value;
+  });
+};
+
+//? function to convert RGBA to hex
+const rgbaFromHex = (hexColor) => {
+  return (
+    [
+      parseInt(hexColor.slice(1, 3), 16),
+      parseInt(hexColor.slice(3, 5), 16),
+      parseInt(hexColor.slice(5, 7), 16),
+    ],
+    join(",")
+  );
+};
+
+// ? function to set the background color
+
+const setTypeBackgroundColor = (pokemon) => {
+  //    ? define a maintype
+  const mainType = pokemon.types[0].type.name;
+  //   ? define the color
+  const color = typeColors[mainType];
+
+  // ? conditional to check if there is not color
+  if (!color) {
+    console.warn(`Color not defined for type:${mainType} `);
+    return;
+  }
+
+  // ? set the background color
+  const detailMainElement = document.querySelector(".detail-main");
+  setElementStyles([detailMainElement], "backgroundColor", color);
+
+  setElementStyles([detailMainElement], "borderColor", color);
+
+  setElementStyles(
+    document.querySelectorAll(".power-wrapper > p"),
+    "backgroundColor",
+    color
+  );
+  setElementStyles(
+    document.querySelectorAll(".stats-wrap p.stats"),
+    "color",
+    color
+  );
+  setElementStyles(
+    document.querySelectorAll(".stats-wrap .progress-bar"),
+    "color",
+    color
+  );
+
+  //? define rgba color and style tag
+  const rgbaColor = rgbaFromHex(color);
+  const styleTag = document.createElement("style");
+
+  style.innerHTML = `
+   .stats-wrap .progress-bar::webkit-progress-bar {
+       background-color: rgba(${rgbaColor},0.5);
+   }
+   .stats-wrap .progress-bar::webkit-progress-value {
+       background-color: ${color};
+   }
+
+`;
+  // ?append the styles
+
+  document.appendChild(styleTag);
+};
